3f937470d0d2e18a0b81fe303551513d62a89b20
Roman F <wavystyledev@gmail.com>
2024-03-26 21:05:45 -0400

    styled desktop navbar

--- app/ui/components/Navigation/Desktop.tsx
+++ app/ui/components/Navigation/Desktop.tsx
@@ -12,13 +12,20 @@ export function DesktopNav({
   lang: Locale;
 }) {
   return (
-    <div className={cn(`${className}`)}>
-      <Wrapper>
-        <div className="flex justify-between items-center w-full">
-          <Brand />
-          <ToggleClose />
-        </div>
-      </Wrapper>
-    </div>
+    <>
+      <div
+        className={cn(
+          `bg-white sticky top-0 shadow-[0.1rem_0.1rem_0.25rem_-0.05rem_rgba(79,139,194,0.5)] ${className}`
+        )}
+      >
+        <Wrapper>
+          <div className="flex justify-between items-center w-full">
+            <Brand />
+            <ToggleClose />
+          </div>
+        </Wrapper>
+      </div>
+      <div className="h-2 bg-white" />
+    </>
   );
 }
